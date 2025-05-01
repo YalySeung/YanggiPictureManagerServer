ddb6477932f34de85eda5d2176084edf1a78ac67
YalySeung <yalyseung@gmail.com>
2025-05-01 10:46:26 +0900

    프로젝트 최초 커밋

---
+++ src/main/java/com/yanggi/yanggipicturemanagerserver/YanggiPictureManagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.yanggi.yanggipicturemanagerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanggiPictureManagerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YanggiPictureManagerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yanggi/yanggipicturemanagerserver/YanggiPictureManagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanggi.yanggipicturemanagerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanggiPictureManagerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
